ee4d8eb2b4439ad80b708191a976e63abad322fb
Morris Janatzek <mjanatzek@brockhaus-ag.de>
2019-02-08 17:45:23 +0100

    updated readme and public api.ts

--- projects/ng-metro4/src/public_api.ts
+++ projects/ng-metro4/src/public_api.ts
@@ -3,16 +3,52 @@
  */
 
 export * from './lib/ng-metro4.module';
-export * from './lib/base/ng-metro4-base.module';
-export * from './lib/form/ng-metro4-forms.module';
-export * from './lib/controls/ng-metro4-controls.module';
 
+export * from './lib/base/ng-metro4-base.module';
+export * from './lib/base/icon/icon.component';
+export * from './lib/base/button/button.directive';
+export * from './lib/base/ripple/ripple.directive';
+
+export * from './lib/form/ng-metro4-forms.module';
+export * from './lib/form/calendar/calendar.component';
+export * from './lib/form/calendar-picker/calendar-picker.component';
+export * from './lib/form/checkbox/checkbox.component';
+export * from './lib/form/checkbox-group/checkbox-group.component';
+export * from './lib/form/date-picker/date-picker.component';
+export * from './lib/form/file-input/file-input.component';
+export * from './lib/form/input/input.component';
+export * from './lib/form/keypad/keypad.component';
+export * from './lib/form/material-input/material-input.component';
+export * from './lib/form/radio/radio.component';
+export * from './lib/form/radio-group/radio-group.component';
+export * from './lib/form/rating/rating.component';
+export * from './lib/form/select/select.component';
+export * from './lib/form/slider/slider.component';
+export * from './lib/form/spinner/spinner.component';
+export * from './lib/form/switch/switch.component';
+export * from './lib/form/tag-input/tag-input.component';
+export * from './lib/form/textarea/textarea.component';
+export * from './lib/form/time-picker/time-picker.component';
+
+export * from './lib/controls/ng-metro4-controls.module';
 export * from './lib/controls/activity/activity.service';
+export * from './lib/controls/donut/donut.component';
+export * from './lib/controls/gravatar/gravatar.component';
+export * from './lib/controls/panel/panel.component';
+export * from './lib/controls/popover/popover.directive';
+export * from './lib/controls/progress/progress.component';
+
+export * from './lib/information/ng-metro4-information.module';
 export * from './lib/information/toast/toast.service';
 export * from './lib/information/notify/notify.service';
 export * from './lib/information/dialog/dialog.service';
-
+export * from './lib/information/hint/hint.directive';
 export * from './lib/information/dialog/dialog.component';
 
 export * from './lib/helper/types';
 export * from './lib/helper/lists';
+export * from './lib/helper/array-helper';
+export * from './lib/helper/attribute-helper';
+export * from './lib/helper/object-helper';
+export * from './lib/helper/compare-helper';
+export * from './lib/helper/string-helper';
